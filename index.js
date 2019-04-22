f2f833dd9c935ad6af63c1bc10e875cfb6c7e41b
SamyakLearner <43209060+SamyakLearner@users.noreply.github.com>
2019-04-23 01:01:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const socket = require('socket.io');
 
 // App setup
 const app = express();
-let server = app.listen(4000,function(){
+const server = app.listen(4000,function(){
     console.log('listening to requests on port 4000');
 });
 
